397aba65af2e03e3282a03903df02fc1c1415dbc
MessineB <messinebelaroussi@gmail.com>
2021-12-09 14:47:11 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,35 @@
+let ajouter = document.querySelector('.ajouter');
+let input = document.querySelector('.input');
+
+
+
+
+function ajouterunepersonne () {
+    let nouveau_nom = input.value;
+    if (localStorage.getItem('nom') == null ) {
+        localStorage.setItem('nom' , '[]');
+    }
+    let ancien_nom = JSON.parse (localStorage.getItem('nom'));
+    
+    ancien_nom.push(nouveau_nom);
+
+    localStorage.setItem('nom',JSON.stringify(ancien_nom));
+    input.value = "" ;
+    let ul = document.getElementById("output")
+    ul.innerHTML="" ;
+    
+    if (localStorage.getItem('nom') != null ) {
+        ancien_nom.forEach(element => {
+            let elem = document.createElement('li');
+            elem.innerHTML = element;
+            ul.appendChild(elem);   
+            
+        });
+
+
+    }
+
+
+
+}
+
